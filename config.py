b50522631d0782ae4a44c1bf15cf37b898b0aee3
Bssamjh <112725901+Bssamjh@users.noreply.github.com>
2022-12-14 19:50:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ from os import getenv
 API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5201596329").split()))
+OWNER_ID = int(getenv("OWNER_ID", "5201596329")) #ur id
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://bssamfor4:zaid@cluster0.wqzjo7d.mongodb.net/?retryWrites=true&w=majority") # an database
+BOT_TOKEN = getenv("BOT_TOKEN", "5899569852:AAEzpgZZu4hq4Kp18N6M7YuHKTNg4Q0KMVE")
 ALIVE_PIC = getenv("ALIVE_PIC")
-ALIVE_TEXT = getenv("ALIVE_TEXT")
-PM_LOGGER = getenv("PM_LOGGER")
-LOG_GROUP = getenv("LOG_GROUP")
+ALIVE_TEXT = getenv("ALIVE_TEXT") #optional
+PM_LOGGER = getenv("PM_LOGGER") # i'd if uh want 
+LOG_GROUP = getenv("LOG_GROUP") #id if uh want enable tagaiert
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "AQBiMZkAvbD_rvhwuFW3eHwEP-mI6E84v8mIkfjp-jH249q2Xg9QFu4gaBC5mMMaWn-X6aKLKbWoko1UoRkKn8oo5LKQwxkBiCZv0dbkZ6QKeEGXJCyT9Ciiq1WOfC9KJyQpAHC_0tprIfWc_3b7fXzAVBDTPrFnDkQEk2qCGlOpNNIE-STDunIy6KR7hg1O0tXeeSnWhNI6YD81Ed2L7KHlMXNeOVZI-P5ulvpqUVwXNsYMtC9_3uMhuXczLn-z5CfRdVsCJz00Xg1HN4Nxg4icAFa6EIOZlFt5lAVCifhXyr7mIxYu_F607JdesOXXtQjwmGdn4SV93U-tfLi4USAsp5lo4AAAAAE2Cg-pAA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
